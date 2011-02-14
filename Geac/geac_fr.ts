66e85e36072e4462bd4d1877f86e49fd4c112a05
Emmanuel Nicolas <emmanuel.nicolas_svn@polytechnique.org>
2011-02-15 00:23:59 +0100

    Ajout Traduction

---
+++ Geac/geac_fr.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Geac</name>
+    <message>
+        <location filename="geac.cpp" line="16"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="geac.cpp" line="29"/>
+        <source>Open Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="20"/>
+        <source>Geac</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="34"/>
+        <source>File&amp;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="52"/>
+        <source>&amp;Open File...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="55"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="63"/>
+        <source>Open &amp;Folder...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="66"/>
+        <source>Ctrl+F</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
