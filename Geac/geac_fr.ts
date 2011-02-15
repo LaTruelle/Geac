e63850da4dd8321b06ca36decac2daa32726b95f
Emmanuel Nicolas <emmanuel.nicolas_svn@polytechnique.org>
2011-02-16 00:48:57 +0100

    Translation to French

--- Geac/geac_fr.ts
+++ Geac/geac_fr.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="fr_FR">
 <context>
     <name>Geac</name>
     <message>
         <location filename="geac.cpp" line="16"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir un Fichier</translation>
     </message>
     <message>
         <location filename="geac.cpp" line="29"/>
         <source>Open Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvir un Dossier</translation>
     </message>
     <message>
         <location filename="geac.ui" line="20"/>
         <source>Geac</source>
-        <translation type="unfinished"></translation>
+        <translation>Geac</translation>
     </message>
     <message>
         <location filename="geac.ui" line="34"/>
         <source>File&amp;</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier</translation>
     </message>
     <message>
         <location filename="geac.ui" line="52"/>
         <source>&amp;Open File...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Ouvrir un Fichier...</translation>
     </message>
     <message>
         <location filename="geac.ui" line="55"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="geac.ui" line="63"/>
         <source>Open &amp;Folder...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir un &amp;Dossier...</translation>
     </message>
     <message>
         <location filename="geac.ui" line="66"/>
         <source>Ctrl+F</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
