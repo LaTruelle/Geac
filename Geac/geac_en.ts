16711dd90109ffea7973fb21e622af27142ff44f
Emmanuel Nicolas <emmanuel.nicolas_svn@polytechnique.org>
2011-02-22 23:45:38 +0100

    Translated to this point

--- Geac/geac_en.ts
+++ Geac/geac_en.ts
@@ -1,47 +1,113 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="en" sourcelanguage="en">
 <context>
     <name>Geac</name>
     <message>
-        <location filename="geac.cpp" line="27"/>
-        <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <location filename="geac.cpp" line="54"/>
+        <source>Open Folder</source>
+        <translation>Open Folder</translation>
     </message>
     <message>
-        <location filename="geac.cpp" line="35"/>
-        <source>Open Folder</source>
-        <translation type="unfinished"></translation>
+        <location filename="geac.cpp" line="93"/>
+        <source>Store ESI in this directory</source>
+        <translation>Store ESI in this directory</translation>
     </message>
     <message>
         <location filename="geac.ui" line="14"/>
         <source>Geac</source>
-        <translation type="unfinished"></translation>
+        <translation>Geac</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="45"/>
+        <location filename="geac.ui" line="37"/>
+        <source>In Same Folder</source>
+        <translation>In Same Folder</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="44"/>
+        <source>In Dedicated Folder</source>
+        <translation>In Dedicated Folder</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="54"/>
+        <location filename="geac.ui" line="61"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="68"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="79"/>
+        <source>_ESI.txt</source>
+        <translation>_ESI.txt</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="86"/>
+        <source>ESI File Extension</source>
+        <translation>ESI File Extension</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="110"/>
+        <source>Harmonic Frequencies</source>
+        <translation>Harmonic Frequencies</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="117"/>
+        <source>Hartree Fock Energy</source>
+        <translation>Hartree Fock Energy</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="124"/>
+        <source>Data to Include</source>
+        <translation>Data to include</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="131"/>
+        <source>Thermochemistry</source>
+        <translation>Thermochemistry</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="138"/>
+        <source>Standard Coordinates</source>
+        <translation>Standard Coordinates</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="149"/>
+        <source>Clear Files</source>
+        <translation>Clear Files</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="169"/>
+        <source>Create ESI</source>
+        <translation>Create ESI</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="195"/>
         <source>File&amp;</source>
-        <translation type="unfinished"></translation>
+        <translation>File</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="63"/>
+        <location filename="geac.ui" line="213"/>
         <source>&amp;Open File...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Open File...</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="66"/>
+        <location filename="geac.ui" line="216"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="geac.ui" line="74"/>
+        <location filename="geac.ui" line="224"/>
         <source>Open &amp;Folder...</source>
-        <translation type="unfinished"></translation>
+        <translation>Open &amp;Folder...</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="77"/>
+        <location filename="geac.ui" line="227"/>
         <source>Ctrl+F</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
--- Geac/geac_fr.ts
+++ Geac/geac_fr.ts
@@ -4,14 +4,18 @@
 <context>
     <name>Geac</name>
     <message>
-        <location filename="geac.cpp" line="27"/>
         <source>Open File</source>
-        <translation>Ouvrir un Fichier</translation>
+        <translation type="obsolete">Ouvrir un Fichier</translation>
     </message>
     <message>
-        <location filename="geac.cpp" line="35"/>
+        <location filename="geac.cpp" line="54"/>
         <source>Open Folder</source>
         <translation>Ouvir un Dossier</translation>
+    </message>
+    <message>
+        <location filename="geac.cpp" line="93"/>
+        <source>Store ESI in this directory</source>
+        <translation>Enregistrer l&apos;ESI dans ce dossier</translation>
     </message>
     <message>
         <location filename="geac.ui" line="14"/>
@@ -19,27 +23,93 @@
         <translation>Geac</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="45"/>
+        <location filename="geac.ui" line="37"/>
+        <source>In Same Folder</source>
+        <translation>Dans le même dossier</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="44"/>
+        <source>In Dedicated Folder</source>
+        <translation>Dans un dossier dédié</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="54"/>
+        <location filename="geac.ui" line="61"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="68"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="79"/>
+        <source>_ESI.txt</source>
+        <translation>_ESI.txt</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="86"/>
+        <source>ESI File Extension</source>
+        <translation>Extension par défaut</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="110"/>
+        <source>Harmonic Frequencies</source>
+        <translation>Fréquences harmoniques</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="117"/>
+        <source>Hartree Fock Energy</source>
+        <translation>Energie Hartree Fock</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="124"/>
+        <source>Data to Include</source>
+        <translation>Données à inclure</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="131"/>
+        <source>Thermochemistry</source>
+        <translation>Thermochimie</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="138"/>
+        <source>Standard Coordinates</source>
+        <translation>Coordonnées Standard</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="149"/>
+        <source>Clear Files</source>
+        <translation>Supprimer les fichiers</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="169"/>
+        <source>Create ESI</source>
+        <translation>Créer l&apos;ESI</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="195"/>
         <source>File&amp;</source>
         <translation>Fichier</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="63"/>
+        <location filename="geac.ui" line="213"/>
         <source>&amp;Open File...</source>
         <translation>&amp;Ouvrir un Fichier...</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="66"/>
+        <location filename="geac.ui" line="216"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="geac.ui" line="74"/>
+        <location filename="geac.ui" line="224"/>
         <source>Open &amp;Folder...</source>
         <translation>Ouvrir un &amp;Dossier...</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="77"/>
+        <location filename="geac.ui" line="227"/>
         <source>Ctrl+F</source>
         <translation></translation>
     </message>
