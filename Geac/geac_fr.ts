c92223eee20c29df8c591f6a94bbfcf3133a05b4
Emmanuel Nicolas <emmanuel.nicolas_svn@polytechnique.org>
2011-05-15 14:00:27 +0200

    Update Translation

--- Geac/geac_fr.ts
+++ Geac/geac_fr.ts
@@ -2,20 +2,72 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR">
 <context>
+    <name>CheckFileDialog</name>
+    <message>
+        <location filename="checkfiledialog.cpp" line="9"/>
+        <source>All Files (*.*)</source>
+        <translation>Tous les Fichiers (*.*)</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="10"/>
+        <source>All Gaussian Output Files (*.out *.log)</source>
+        <translation>Tous les fichiers de Sorties Gaussian (*.out, *.log)</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="11"/>
+        <source>Gaussian out files (*.out)</source>
+        <translation>Fichiers out Gaussian (*.out)</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="12"/>
+        <source>Gaussian log files (*.log)</source>
+        <translation>Fichiers de Sortie Gausian (*.log)</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="40"/>
+        <source>Search Subfolders</source>
+        <translation>Explorer les sous-dossiers</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="42"/>
+        <source>All Files</source>
+        <translation>Tous les Fichiers</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="43"/>
+        <source>Out and Log Files</source>
+        <translation>Fichiers Out et Log</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="44"/>
+        <source>Out Files Only</source>
+        <translation>Fichiers Out seulement</translation>
+    </message>
+    <message>
+        <location filename="checkfiledialog.cpp" line="45"/>
+        <source>Log Files Only</source>
+        <translation>Fichiers Log seulement</translation>
+    </message>
+</context>
+<context>
     <name>Geac</name>
     <message>
         <source>Open File</source>
         <translation type="obsolete">Ouvrir un Fichier</translation>
     </message>
     <message>
-        <location filename="geac.cpp" line="54"/>
         <source>Open Folder</source>
-        <translation>Ouvir un Dossier</translation>
+        <translation type="obsolete">Ouvir un Dossier</translation>
     </message>
     <message>
-        <location filename="geac.cpp" line="93"/>
+        <location filename="geac.cpp" line="184"/>
         <source>Store ESI in this directory</source>
         <translation>Enregistrer l&apos;ESI dans ce dossier</translation>
+    </message>
+    <message>
+        <location filename="geac.cpp" line="196"/>
+        <source>Files Cleared</source>
+        <translation>Fichiers Supprimés</translation>
     </message>
     <message>
         <location filename="geac.ui" line="14"/>
@@ -28,90 +80,117 @@
         <translation>Dans le même dossier</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="44"/>
+        <location filename="geac.ui" line="47"/>
         <source>In Dedicated Folder</source>
         <translation>Dans un dossier dédié</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="54"/>
-        <location filename="geac.ui" line="61"/>
-        <source>TextLabel</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="geac.ui" line="68"/>
+        <location filename="geac.ui" line="57"/>
+        <location filename="geac.ui" line="64"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="79"/>
+        <location filename="geac.ui" line="75"/>
         <source>_ESI.txt</source>
         <translation>_ESI.txt</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="86"/>
+        <location filename="geac.ui" line="82"/>
         <source>ESI File Extension</source>
         <translation>Extension par défaut</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="110"/>
+        <location filename="geac.ui" line="106"/>
         <source>Harmonic Frequencies</source>
         <translation>Fréquences harmoniques</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="117"/>
+        <location filename="geac.ui" line="113"/>
         <source>Hartree Fock Energy</source>
         <translation>Energie Hartree Fock</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="124"/>
+        <location filename="geac.ui" line="120"/>
         <source>Data to Include</source>
         <translation>Données à inclure</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="131"/>
+        <location filename="geac.ui" line="127"/>
         <source>Thermochemistry</source>
         <translation>Thermochimie</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="138"/>
+        <location filename="geac.ui" line="134"/>
         <source>Standard Coordinates</source>
         <translation>Coordonnées Standard</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="149"/>
+        <location filename="geac.ui" line="145"/>
         <source>Clear Files</source>
         <translation>Supprimer les fichiers</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="169"/>
+        <location filename="geac.ui" line="165"/>
         <source>Create ESI</source>
         <translation>Créer l&apos;ESI</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="195"/>
+        <location filename="geac.ui" line="191"/>
         <source>File&amp;</source>
         <translation>Fichier</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="213"/>
+        <location filename="geac.ui" line="200"/>
+        <source>toolBar</source>
+        <translation>Barre d&apos;Outils</translation>
+    </message>
+    <message>
+        <location filename="geac.ui" line="223"/>
         <source>&amp;Open File...</source>
         <translation>&amp;Ouvrir un Fichier...</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="216"/>
+        <location filename="geac.ui" line="226"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="geac.ui" line="224"/>
+        <location filename="geac.ui" line="238"/>
         <source>Open &amp;Folder...</source>
         <translation>Ouvrir un &amp;Dossier...</translation>
     </message>
     <message>
-        <location filename="geac.ui" line="227"/>
+        <location filename="geac.ui" line="241"/>
         <source>Ctrl+F</source>
         <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="esiextractor.cpp" line="22"/>
+        <location filename="esiextractor.cpp" line="29"/>
+        <source>The file </source>
+        <translation>Le fichier </translation>
+    </message>
+    <message>
+        <location filename="esiextractor.cpp" line="22"/>
+        <source> does not exist !</source>
+        <translation> n&apos;existe pas !</translation>
+    </message>
+    <message>
+        <location filename="esiextractor.cpp" line="29"/>
+        <source> cannot be opened !</source>
+        <translation> ne peut pas être ouvert !</translation>
+    </message>
+</context>
+<context>
+    <name>void::Geac</name>
+    <message>
+        <location filename="geac.cpp" line="145"/>
+        <source> Added</source>
+        <translation> ajouté</translation>
     </message>
 </context>
 </TS>
