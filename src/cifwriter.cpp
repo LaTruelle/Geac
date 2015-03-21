aed66ab77e4ce84991d66692f773557bf4e9a05d
Emmanuel Nicolas <emmanuel.nicolas_svn@polytechnique.org>
2015-03-21 18:53:54 +0100

    CIF File is valid
    Not the best ever but definitely valid

--- src/cifwriter.cpp
+++ src/cifwriter.cpp
@@ -30,9 +30,6 @@ void CifWriter::setOutputFile(QFile &file)
 
 void CifWriter::createCif()
 {
-    qDebug() << "Entering Create CIF";
-    qDebug() << outputFile.fileName();
-    qDebug() << "List: " + files.first()->fileName();
     // The main function that actually does the job!
     // TODO: Checks for existence of files and everything
     // Open the file
@@ -41,14 +38,29 @@ void CifWriter::createCif()
     QTextStream out(&outputFile);
     // Add data according to requirements
     out << "Starting CIF File" << endl;
+
     for (int i = 0; i < files.size(); ++i) {
         // Add each file as a new cif block
         QString fileName = files.at(i)->fileName().right(files.at(i)->fileName().size()
                                                         - files.at(i)->fileName().lastIndexOf("/")
                                                         - 1);
         QList<QStringList> XYZCoordinates = files.at(i)->getXYZCoordinates();
-        out << "_file " + fileName << endl;
-        out << "__ XYZ Coordinates __" << endl;
+        out << "data_" + fileName << endl;
+        out << endl;
+        out << "_cell_length_a                    1.00" << endl;
+        out << "_cell_length_b                    1.00" << endl;
+        out << "_cell_length_c                    1.00" << endl;
+        out << "_cell_angle_alpha                90.00" << endl;
+        out << "_cell_angle_beta                 90.00" << endl;
+        out << "_cell_angle_gamma                90.00" << endl;
+        out << "_cell_volume                      1.00" << endl;
+        out << "_cell_formula_units_Z             1" << endl;
+        out << endl;
+        out << "loop_" << endl;
+        out << "_atom_site_type_symbol" << endl;
+        out << "_atom_site_fract_x" << endl;
+        out << "_atom_site_fract_y" << endl;
+        out << "_atom_site_fract_z" << endl;
         for (int j = 0; j < files.at(i)->getNAtoms().toInt(); ++j) {
             QStringList line = XYZCoordinates.at(j);
             QString formattedLine;
@@ -58,7 +70,9 @@ void CifWriter::createCif()
             formattedLine += line.at(3).rightJustified(16,' ');
             out << formattedLine << endl;
         }
-        out << "__end of " + fileName << endl;
+        out << endl;
+        out << "# end of " + fileName << endl;
+        out << endl;
     }
     out << "__ Full End " << endl;
     outputFile.close();
